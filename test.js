e5388f7397128e8073045609e2f3e756108ddc62
Titus <tituswormer@gmail.com>
2019-05-31 08:34:58 +0200

    Change signature to use `node` first, `test` second
    Closes GH-6.
    Closes GH-8.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(false)
+      is(null, false)
     },
     /Expected function, string, or object as test/,
     'should throw when `test` is invalid'
@@ -17,7 +17,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, -1, parent)
+      is(node, null, -1, parent)
     },
     /Expected positive finite index or child node/,
     'should throw when `index` is invalid (#1)'
@@ -25,7 +25,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, Infinity, parent)
+      is(node, null, Infinity, parent)
     },
     /Expected positive finite index or child node/,
     'should throw when `index` is invalid (#2)'
@@ -33,7 +33,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, false, parent)
+      is(node, null, false, parent)
     },
     /Expected positive finite index or child node/,
     'should throw when `index` is invalid (#3)'
@@ -41,7 +41,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, 0, {})
+      is(node, null, 0, {})
     },
     /Expected parent node/,
     'should throw when `parent` is invalid (#1)'
@@ -49,9 +49,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, 0, {
-        type: 'paragraph'
-      })
+      is(node, null, 0, {type: 'paragraph'})
     },
     /Expected parent node/,
     'should throw when `parent` is invalid (#2)'
@@ -59,7 +57,7 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, 0)
+      is(node, null, 0)
     },
     /Expected both parent and index/,
     'should throw `parent` xor `index` are given (#1)'
@@ -67,32 +65,32 @@ test('unist-util-is', function(t) {
 
   t.throws(
     function() {
-      is(null, node, null, parent)
+      is(node, null, null, parent)
     },
     /Expected both parent and index/,
     'should throw `parent` xor `index` are given (#2)'
   )
 
   t.notok(is(), 'should not fail without node')
-  t.ok(is(null, node), 'should check if given a node (#1)')
-  t.notok(is(null, {children: []}), 'should check if given a node (#2)')
+  t.ok(is(node), 'should check if given a node (#1)')
+  t.notok(is({children: []}, null), 'should check if given a node (#2)')
 
-  t.ok(is('strong', node), 'should match types (#1)')
-  t.notok(is('emphasis', node), 'should match types (#2)')
+  t.ok(is(node, 'strong'), 'should match types (#1)')
+  t.notok(is(node, 'emphasis'), 'should match types (#2)')
 
   t.ok(is(node, node), 'should match partially (#1)')
-  t.ok(is({type: 'strong'}, node), 'should match partially (#2)')
-  t.ok(is({type: 'paragraph'}, parent), 'should match partially (#3)')
-  t.notok(is({type: 'paragraph'}, node), 'should match partially (#4)')
+  t.ok(is(node, {type: 'strong'}), 'should match partially (#2)')
+  t.ok(is(parent, {type: 'paragraph'}), 'should match partially (#3)')
+  t.notok(is(node, {type: 'paragraph'}), 'should match partially (#4)')
 
   t.test('should accept a test', function(st) {
     function test(node, n) {
       return n === 5
     }
 
-    t.notok(is(test, node))
-    t.notok(is(test, node, 0, parent))
-    st.ok(is(test, node, 5, parent))
+    t.notok(is(node, test))
+    t.notok(is(node, test, 0, parent))
+    st.ok(is(node, test, 5, parent))
 
     st.end()
   })
@@ -109,18 +107,18 @@ test('unist-util-is', function(t) {
       st.equal(c, parent)
     }
 
-    is(test, node, 5, parent, context)
+    is(node, test, 5, parent, context)
   })
 
-  t.ok(is(['strong', 'emphasis'], node), 'should match arrays (#1)')
-  t.notok(is(['b', 'i'], node), 'should match arrays (#2)')
+  t.ok(is(node, ['strong', 'emphasis']), 'should match arrays (#1)')
+  t.notok(is(node, ['b', 'i']), 'should match arrays (#2)')
 
   t.test('should match arrays (#3)', function(st) {
     var context = {foo: 'bar'}
 
     st.plan(5)
 
-    st.ok(is([test, 'strong'], node, 5, parent, context))
+    st.ok(is(node, [test, 'strong'], 5, parent, context))
 
     function test(a, b, c) {
       st.equal(this, context)
